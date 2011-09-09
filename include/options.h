5b1e38b1d29384f47c5e53f5169867767389d5de
Jon Stewart <jon@lightboxtechnologies.com>
2011-09-09 17:03:50 -0400

    Split up the unit tests from the command-line executable. There's now a separate test exe.

---
+++ include/options.h
@@ -0,0 +1,35 @@
+#pragma once
+
+// must include <fstream> before this header, because of <iosfwd> usage
+
+#include "basic.h"
+#include <iosfwd>
+
+class Options {
+public:
+  uint64  DebugBegin,
+          DebugEnd;
+
+  std::string KeyFile,
+              Command,
+              Input,
+              Output,
+              Encoding,
+              Pattern,
+              ServerLog;
+
+  unsigned int BlockSize;
+
+  bool    CaseSensitive,
+          LiteralMode,
+          NoOutput,
+          PrintPath;
+
+  mutable std::ofstream OutputFile;
+
+  uint32 getEncoding() const;
+
+  std::ostream& openOutput() const;
+
+  std::vector< std::string > getKeys() const;
+};
