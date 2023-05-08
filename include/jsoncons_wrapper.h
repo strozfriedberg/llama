517cc34efe7edec49809768a8b6eb13a827387ba
Jon Stewart <jstewart@strozfriedberg.com>
2023-05-08 12:25:00 -0400

    r - dear compiler: stfu about jsoncons and it's unused parameters

--- include/jsoncons_wrapper.h
+++ include/jsoncons_wrapper.h
@@ -2,5 +2,6 @@
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wreturn-type"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 #include <jsoncons/json.hpp>
 #pragma GCC diagnostic pop
