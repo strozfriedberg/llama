d0110f76485b4be21c6904552e36bb39e7520779
Jon Stewart <jstewart@strozfriedberg.com>
2023-05-07 22:27:13 -0400

    r - move TskReader::startReading() fromo header to source file.

--- include/tskreader.h
+++ include/tskreader.h
@@ -40,31 +40,7 @@ public:
     Output = out;
   }
 
-  virtual bool startReading() override {
-    Asm.addImage(Tsk->convertImg(*Img));
-
-    // tell TskAuto to start giving files to processFile
-    // std::cerr << "Image is " << getImageSize() << " bytes in size" << std::endl;
-    const bool ret = Tsk->walk(
-      Img.get(),
-      [this](const TSK_VS_INFO* vs_info) { return filterVs(vs_info); },
-      [this](const TSK_VS_PART_INFO* vs_part) { return filterVol(vs_part); },
-      [this](TSK_FS_INFO* fs_info) { return filterFs(fs_info); },
-      [this](TSK_FS_FILE* fs_file, const char* path) { return processFile(fs_file, path); }
-    );
-
-    if (ret) {
-      // wrap up the walk
-      while (!Dirents.empty()) {
-        Output->outputDirent(Dirents.pop());
-      }
-      Output->outputImage(Asm.dump());
-
-      // teardown
-      Input->flush();
-    }
-    return ret;
-  }
+  virtual bool startReading() override;
 
 private:
   // callbacks
--- src/llama/tskreader.cpp
+++ src/llama/tskreader.cpp
@@ -17,3 +17,29 @@ TskReader::TskReader(const std::string& imgPath):
 bool TskReader::open() {
   return bool(Img = Tsk->openImg(ImgPath.c_str()));
 }
+
+bool TskReader::startReading() {
+  Asm.addImage(Tsk->convertImg(*Img));
+
+  // tell TskAuto to start giving files to processFile
+  // std::cerr << "Image is " << getImageSize() << " bytes in size" << std::endl;
+  const bool ret = Tsk->walk(
+    Img.get(),
+    [this](const TSK_VS_INFO* vs_info) { return filterVs(vs_info); },
+    [this](const TSK_VS_PART_INFO* vs_part) { return filterVol(vs_part); },
+    [this](TSK_FS_INFO* fs_info) { return filterFs(fs_info); },
+    [this](TSK_FS_FILE* fs_file, const char* path) { return processFile(fs_file, path); }
+  );
+
+  if (ret) {
+    // wrap up the walk
+    while (!Dirents.empty()) {
+      Output->outputDirent(Dirents.pop());
+    }
+    Output->outputImage(Asm.dump());
+
+    // teardown
+    Input->flush();
+  }
+  return ret;
+}
