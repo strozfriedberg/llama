80191f403a64dd64d6aa418b53f6c11399a176f7
Jon Stewart <jstewart@strozfriedberg.com>
2023-05-16 13:53:55 -0400

    r - move nontrivial method bodies of FileBlockSequence and TskBlockSequence into the source file.

--- include/blocksequence_impl.h
+++ include/blocksequence_impl.h
@@ -13,41 +13,18 @@
 
 class FileBlockSequence: public BlockSequence {
 public:
-  FileBlockSequence(const std::string& path):
-    path(path),
-    off(0),
-    rlen(0),
-    bufend(&buf[0])
-  {
-  }
+  FileBlockSequence(const std::string& path);
 
   virtual ~FileBlockSequence() {}
 
-  virtual Iterator begin() override {
-    in.open(path, std::ios::binary);
-    return in ? Iterator(this) : end();
-  }
+  virtual Iterator begin() override;
 
 protected:
-  virtual bool advance() override {
-    off += rlen;
+  virtual bool advance() override;
 
-    const bool ok = bool(in);
+  virtual std::pair<const uint8_t*, const uint8_t*> cur() const override;
 
-    in.read(reinterpret_cast<char*>(buf), sizeof(buf));
-    rlen = in.gcount();
-    bufend = buf + rlen;
-
-    return ok;
-  }
-
-  virtual std::pair<const uint8_t*, const uint8_t*> cur() const override {
-    return std::make_pair(&buf[0], bufend);
-  }
-
-  virtual size_t offset() const override {
-    return off;
-  }
+  virtual size_t offset() const override;
 
 private:
   std::string path;
@@ -59,50 +36,15 @@ private:
   uint8_t* bufend;
 };
 
+
 class TskBlockSequence: public BlockSequence {
 public:
-  TskBlockSequence(std::unique_ptr<TSK_FS_FILE, void(*)(TSK_FS_FILE*)> f):
-    file(std::move(f)),
-    off(0),
-    rlen(0)
-  {
-    // TODO: could this return null?
-    const TSK_FS_ATTR* attr = tsk_fs_file_attr_get(file.get());
-    size = attr ? attr->size : 0;
-  }
+  TskBlockSequence(std::unique_ptr<TSK_FS_FILE, void(*)(TSK_FS_FILE*)> f);
 
   virtual ~TskBlockSequence() {}
 
 protected:
-  virtual bool advance() override {
-    // TODO: check that size and final offset comport
-
-    // advance offset by the previous read length
-    off += rlen;
-
-    if (off >= size) {
-      bufend = &buf[0];
-      return false;
-    }
-
-    // fill the buffer
-    rlen = tsk_fs_file_read(
-      file.get(), off,
-      reinterpret_cast<char*>(buf), sizeof(buf),
-      TSK_FS_FILE_READ_FLAG_NONE
-    );
-
-    // mark the end of the data read
-    if (rlen < 0) {
-      std::cerr << "Error: " << tsk_error_get() << std::endl;
-      bufend = &buf[0];
-    }
-    else {
-      bufend = buf + rlen;
-    }
-
-    return bufend > &buf[0];
-  }
+  virtual bool advance() override;
 
   virtual std::pair<const uint8_t*, const uint8_t*> cur() const override {
     return std::make_pair(&buf[0], bufend);
--- src/llama/blocksequence_impl.cpp
+++ src/llama/blocksequence_impl.cpp
@@ -1,1 +1,75 @@
 #include "blocksequence_impl.h"
+
+FileBlockSequence::FileBlockSequence(const std::string& path):
+  path(path),
+  off(0),
+  rlen(0),
+  bufend(&buf[0])
+{
+}
+
+BlockSequence::Iterator FileBlockSequence::begin() {
+  in.open(path, std::ios::binary);
+  return in ? Iterator(this) : end();
+}
+
+bool FileBlockSequence::advance() {
+  off += rlen;
+
+  const bool ok = bool(in);
+
+  in.read(reinterpret_cast<char*>(buf), sizeof(buf));
+  rlen = in.gcount();
+  bufend = buf + rlen;
+
+  return ok;
+}
+
+std::pair<const uint8_t*, const uint8_t*> FileBlockSequence::cur() const {
+  return std::make_pair(&buf[0], bufend);
+}
+
+size_t FileBlockSequence::offset() const {
+  return off;
+}
+//***************************************************************************
+
+TskBlockSequence::TskBlockSequence(std::unique_ptr<TSK_FS_FILE, void(*)(TSK_FS_FILE*)> f):
+  file(std::move(f)),
+  off(0),
+  rlen(0)
+{
+  // TODO: could this return null?
+  const TSK_FS_ATTR* attr = tsk_fs_file_attr_get(file.get());
+  size = attr ? attr->size : 0;
+}
+
+bool TskBlockSequence::advance() {
+  // TODO: check that size and final offset comport
+
+  // advance offset by the previous read length
+  off += rlen;
+
+  if (off >= size) {
+    bufend = &buf[0];
+    return false;
+  }
+
+  // fill the buffer
+  rlen = tsk_fs_file_read(
+    file.get(), off,
+    reinterpret_cast<char*>(buf), sizeof(buf),
+    TSK_FS_FILE_READ_FLAG_NONE
+  );
+
+  // mark the end of the data read
+  if (rlen < 0) {
+    std::cerr << "Error: " << tsk_error_get() << std::endl;
+    bufend = &buf[0];
+  }
+  else {
+    bufend = buf + rlen;
+  }
+
+  return bufend > &buf[0];
+}
