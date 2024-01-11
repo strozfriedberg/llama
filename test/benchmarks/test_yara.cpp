c7ba59a0992b13f5121474466e5ef164609aa15d
Jon Stewart <jstewart@strozfriedberg.com>
2024-01-11 14:37:41 -0500

    t - Add a simple yara benchmark, looking for cobaltstrike beacons in a snippet of a Sherlock Holmes story.

--- test/benchmarks/test_yara.cpp
+++ test/benchmarks/test_yara.cpp
@@ -5,6 +5,18 @@
 
 namespace {
 
+  std::string holmes2(
+R"("My dear fellow," said Sherlock Holmes as we sat on either side of the fire in his lodgings at Baker Street, "life is infinitely stranger than anything which the mind of man could invent. We would not dare to conceive the things which are really mere commonplaces of existence. If we could fly out of that window hand in hand, hover over this great city, gently remove the roofs, and peep in at the queer things which are going on, the strange coincidences, the plannings, the cross-purposes, the wonderful chains of events, working through generations, and leading to the most outre results, it would make all fiction with its conventionalities and foreseen conclusions most stale and unprofitable."
+
+"And yet I am not convinced of it," I answered. "The cases which come to light in the papers are, as a rule, bald enough, and vulgar enough. We have in our police reports realism pushed to its extreme limits, and yet the result is, it must be confessed, neither fascinating nor artistic."
+
+"A certain selection and discretion must be used in producing a realistic effect," remarked Holmes. "This is wanting in the police report, where more stress is laid, perhaps, upon the platitudes of the magistrate than upon the details, which to an observer contain the vital essence of the whole matter. Depend upon it, there is nothing so unnatural as the commonplace."
+
+I smiled and shook my head. "I can quite understand your thinking so." I said. "Of course, in your position of unofficial adviser and helper to everybody who is absolutely puzzled, throughout three continents, you are brought in contact with all that is strange and bizarre. But here"--I picked up the morning paper from the ground--"let us put it to a practical test. Here is the first heading upon which I come. 'A husband's cruelty to his wife.' There is half a column of print, but I know without reading it that it is all perfectly familiar to me. There is, of course, the other woman, the drink, the push, the blow, the bruise, the sympathetic sister or landlady. The crudest of writers could invent nothing more crude."
+
+"Indeed, your example is an unfortunate one for your argument," said Holmes, taking the paper and glancing his eye down it. "This is the Dundas separation case, and, as it happens, I was engaged in clearing up some small points in connection with it. The husband was a teetotaler, there was no other woman, and the conduct complained of was that he had drifted into the habit of winding up every meal by taking out his false teeth and hurling them at his wife, which, you will allow, is not an action likely to occur to the imagination of the average story-teller. Take a pinch of snuff, Doctor, and acknowledge that I have scored over you in your example.\"
+)");
+
 class YaraLib {
 // don't make more than one of these at a time... it would be bad
 public:
@@ -65,6 +77,15 @@ std::string SAMPLE_RULE =
       return 'Z';
     }
   }
+
+  int yara_callback_function(
+    YR_SCAN_CONTEXT* context,
+    int message,
+    void* message_data,
+    void* user_data)
+  {
+    return CALLBACK_CONTINUE;
+  }
 } // local namespace
 
 TEST_CASE("testYara") {
@@ -80,8 +101,8 @@ TEST_CASE("testYara") {
   REQUIRE(0 == yr_compiler_get_rules(comp.get(), &rulesPtr));
   std::shared_ptr<YR_RULES> rules(rulesPtr, yr_rules_destroy);
 
-  REQUIRE(rulesPtr->num_rules == 1);
-  YR_RULE* rule = &((*rulesPtr).rules_table[0]);
+  REQUIRE(rules->num_rules == 1);
+  YR_RULE* rule = &((*rules).rules_table[0]);
   REQUIRE("APT_CobaltStrike_Beacon_Indicator" == std::string(rule->identifier));
 
   std::vector<std::string> patterns;
@@ -98,4 +119,9 @@ TEST_CASE("testYara") {
     }
     patterns.push_back(p);
   }
+
+  int flags = 0;
+  BENCHMARK("yaraHolmes") {
+    REQUIRE(ERROR_SUCCESS == yr_rules_scan_mem(rules.get(), (uint8_t*)holmes2.c_str(), holmes2.size(), flags, yara_callback_function, nullptr, 0));
+  };
 }
