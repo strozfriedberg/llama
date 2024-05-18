67ca143ef3ba024c9f807c89500e44bf0469e419
Jon Stewart <jstewart@strozfriedberg.com>
2024-05-17 22:38:26 -0400

    t - hey, cool, we can insert a couple things into duckdb

--- test/test_duckdb.cpp
+++ test/test_duckdb.cpp
@@ -4,7 +4,27 @@
 
 using namespace duckdb;
 
+struct Dirent {
+  std::string name;
+  std::string path;
+};
+
 TEST_CASE("TestMakeDuckDB") {
   DuckDB db(nullptr);
   Connection dbconn(db);
+
+  auto result = dbconn.Query("CREATE TABLE dirent (name VARCHAR, path VARCHAR);");
+  REQUIRE(!result->HasError());
+
+  std::vector<Dirent> dirents = {
+    {"foo", "/tmp/"},
+    {"bar", "/tmp/"},
+    {"bar", "/temp/"}
+  };
+
+  for (auto dirent : dirents) {
+    result = dbconn.Query("INSERT INTO dirent VALUES ('" + dirent.name + "', '" + dirent.path + "');");
+    REQUIRE(!result->HasError());
+  }
 }
+
