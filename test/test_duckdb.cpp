01f03d5f045f8317ed416a772414945c592d1c89
Jon Stewart <jstewart@strozfriedberg.com>
2024-06-01 07:34:33 -0400

    T - use the duckdb appender API to write data into a table instead of INSERT queries and apply the same SELECT WHERE query. DirentBatch accumulates the paths and filenames of dirents together into one buffer, null-separated, to reduce memory allocations and provide for future lightgrep searching.

--- test/test_duckdb.cpp
+++ test/test_duckdb.cpp
@@ -3,8 +3,26 @@
 #include <duckdb.h>
 
 struct Dirent {
-  std::string name;
   std::string path;
+  std::string name;
+};
+
+struct DirentBatch {
+  size_t size() const { return Offsets.size(); }
+
+  std::vector<char> Buf;
+  std::vector<std::pair<size_t, size_t>> Offsets;
+
+  void add(const Dirent& dent) {
+    size_t pathSize = dent.path.size();
+    size_t totalSize = pathSize + dent.name.size();
+    size_t offset = Buf.size();
+    Buf.resize(offset + totalSize + 1);
+    Offsets.push_back({offset, offset + pathSize});
+    std::copy(dent.path.begin(), dent.path.end(), Buf.begin() + Offsets.back().first);
+    std::copy(dent.name.begin(), dent.name.end(), Buf.begin() + Offsets.back().second);
+    Buf[offset + totalSize] = '\0';
+  }
 };
 
 TEST_CASE("TestMakeDuckDB") { 
@@ -14,22 +32,36 @@ TEST_CASE("TestMakeDuckDB") {
   REQUIRE(duckdb_open(nullptr, &db) == DuckDBSuccess);
   REQUIRE(duckdb_connect(db, &dbconn) == DuckDBSuccess);
 
-
-  auto state = duckdb_query(dbconn, "CREATE TABLE dirent (name VARCHAR, path VARCHAR);", nullptr);
+  auto state = duckdb_query(dbconn, "CREATE TABLE dirent (path VARCHAR, name VARCHAR);", nullptr);
   REQUIRE(state != DuckDBError);
 
   std::vector<Dirent> dirents = {
-    {"foo", "/tmp/"},
-    {"bar", "/tmp/"},
-    {"bar", "/temp/"}
+    {"/tmp/", "foo"},
+    {"/tmp/", "bar"},
+    {"/temp/", "bar"}
   };
 
+  DirentBatch batch;
   std::string query;;
   for (auto dirent : dirents) {
-    query = "INSERT INTO dirent VALUES ('" + dirent.name + "', '" + dirent.path + "');";
-    state = duckdb_query(dbconn, query.c_str(), nullptr);
+    batch.add(dirent);
+  }
+  REQUIRE(batch.size() == 3);
+  REQUIRE(batch.Buf.size() == 28);
+
+  duckdb_appender appender;
+  state = duckdb_appender_create(dbconn, nullptr, "dirent", &appender);
+  REQUIRE(state != DuckDBError);
+  for (auto& offsets: batch.Offsets) {
+    state = duckdb_append_varchar_length(appender, batch.Buf.data() + offsets.first, offsets.second - offsets.first);
+    REQUIRE(state != DuckDBError);
+    state = duckdb_append_varchar(appender, batch.Buf.data() + offsets.second);
+    REQUIRE(state != DuckDBError);
+    state = duckdb_appender_end_row(appender);
     REQUIRE(state != DuckDBError);
   }
+  duckdb_appender_destroy(&appender);
+
   duckdb_result result;
   state = duckdb_query(dbconn, "SELECT * FROM dirent WHERE dirent.path = '/tmp/';", &result);
   REQUIRE(state != DuckDBError);
