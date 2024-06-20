6c9e78c56263018a9a3a44bd70471d9c272b30ff
Jon Stewart <jstewart@strozfriedberg.com>
2024-06-19 21:08:08 -0400

    r - extract the holder classes into their own header file

--- test/test_duckdb.cpp
+++ test/test_duckdb.cpp
@@ -2,7 +2,7 @@
 
 #include "throw.h"
 
-#include <duckdb.h>
+#include "llamaduck.h"
 
 struct Dirent {
   std::string path;
@@ -44,62 +44,6 @@ struct DirentBatch {
   }
 };
 
-class LlamaDB {
-public:
-  LlamaDB(const char* path = nullptr) {
-    auto state = duckdb_open(path, &Db);
-    THROW_IF(state == DuckDBError, "Failed to open database");
-  }
-
-  duckdb_database& get() { return Db; }
-
-  ~LlamaDB() {
-    duckdb_close(&Db);
-  }
-
-private:
-  duckdb_database Db;
-};
-
-class LlamaDBConnection {
-public:
-  LlamaDBConnection(LlamaDB& db) {
-    auto state = duckdb_connect(db.get(), &DBConn);
-    THROW_IF(state == DuckDBError, "Failed to connect to database");
-  }
-  
-  ~LlamaDBConnection() {
-    duckdb_disconnect(&DBConn);
-  }
-
-  duckdb_connection& get() { return DBConn; }
-
-private:
-  LlamaDBConnection(const LlamaDBConnection&) = delete;
-
-  duckdb_connection DBConn;
-};
-
-class LlamaDBAppender {
-public:
-  LlamaDBAppender(duckdb_connection& conn, const char* table) {
-    auto state = duckdb_appender_create(conn, nullptr, table, &Appender);
-    THROW_IF(state == DuckDBError, "Failed to create appender");
-  }
-
-  ~LlamaDBAppender() {
-    duckdb_appender_destroy(&Appender);
-  }
-
-  duckdb_appender& get() { return Appender; }
-
-  bool flush() {
-    return DuckDBSuccess == duckdb_appender_flush(Appender);
-  }
-
-private:
-  duckdb_appender Appender;
-};
 
 TEST_CASE("TestMakeDuckDB") { 
   LlamaDB db;
