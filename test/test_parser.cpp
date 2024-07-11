4b9d6778c0a052a9381661da21cca9a1c306628e
Julia Paluch <paluchj4@gmail.com>
2024-07-11 09:49:12 -0400

    F - add function for peeking at current token

--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -7,6 +7,7 @@ public:
   LlamaParser(std::vector<Token> tokens) : Tokens(tokens) {}
 
   Token previous() { return Tokens.at(CurIdx - 1); }
+  Token peek() const { return Tokens.at(CurIdx); }
 
   std::vector<Token> Tokens;
   uint32_t CurIdx = 0;
@@ -24,4 +25,13 @@ TEST_CASE("TestLlamaParserPrevious") {
   LlamaParser parser(lexer.getTokens());
   parser.CurIdx = 1;
   REQUIRE(parser.previous().Type == TokenType::RULE);
+}
+
+TEST_CASE("TestLlamaParserPeek") {
+  std::string input = "rule { meta: description = \"test\" }";
+  LlamaLexer lexer(input);
+  lexer.scanTokens();
+  LlamaParser parser(lexer.getTokens());
+  parser.CurIdx = 1;
+  REQUIRE(parser.peek().Type == TokenType::OPEN_BRACE);
 }
