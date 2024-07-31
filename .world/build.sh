6ca393da69580e60e03e778dc99f04c87c2208e1
Joel Uckelman <joel.uckelman@aon.com>
2024-07-31 16:37:36 +0100

    F - Don't build shared 64-bit Windows for now.

--- .world/build.sh
+++ .world/build.sh
@@ -2,8 +2,8 @@
 
 . .world/build_config.sh
 
-# don't build 32-bit Windows for now
-if [[ $Target == 'windows' && $Architecture == '32' ]]; then
+# don't build 32-bit or 64-bit shared Windows for now
+if [[ $Target == 'windows' && ($Architecture == '32' || $Linkage == 'shared') ]]; then
   exit
 fi
 
