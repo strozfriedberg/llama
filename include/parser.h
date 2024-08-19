713011e19c94a9f0cf6304431fe1f1a3bb2e532b
Julia Paluch <julia.paluch@strozfriedberg.com>
2024-08-19 00:12:17 -0400

    R - don't reinvent the wheel

--- include/parser.h
+++ include/parser.h
@@ -4,6 +4,8 @@
 #include <lightgrep/api.h>
 #include <lightgrep/util.h>
 
+#include <boost/functional/hash.hpp>
+
 #include <memory>
 #include <unordered_map>
 #include <vector>
@@ -88,11 +90,11 @@ struct SigDefNode : public Node {
 template<>
 struct std::hash<SignatureDef>
 {
-    std::size_t operator()(const SignatureDef& sig) const noexcept
-    {
-        const std::size_t h1 = std::hash<TokenType>{}(sig.Attr);
-        const std::size_t h2 = std::hash<std::string>{}(sig.Val);
-        return h1 ^ ((h2 << 1) >> 1);
+    std::size_t operator()(const SignatureDef& sig) const noexcept {
+      std::size_t hash = 0;
+      boost::hash_combine(hash, std::hash<TokenType>{}(sig.Attr));
+      boost::hash_combine(hash, std::hash<std::string>{}(sig.Val));
+      return hash;
     }
 };
 
@@ -104,17 +106,17 @@ struct FuncNode : public Node {
 template<>
 struct std::hash<ConditionFunction>
 {
-    std::size_t operator()(const ConditionFunction& func) const noexcept
-    {
-        const std::size_t h1 = std::hash<TokenType>{}(func.Name);
-        std::size_t h2 = 0;
-        for (const auto& arg : func.Args) {
-          const std::size_t h = std::hash<std::string>{}(arg);
-          h2 ^= ((h << 1) >> 1);
-        }
-        const std::size_t h3 = std::hash<TokenType>{}(func.Operator);
-        const std::size_t h4 = std::hash<std::string>{}(func.Value);
-        return h1 ^ ((h2 << 1) >> 1) ^ ((h3 << 1) >> 1) ^ ((h4 << 1) >> 1);
+    std::size_t operator()(const ConditionFunction& func) const noexcept {
+      std::size_t hash = 0, h2 = 0;
+      for (const auto& arg : func.Args) {
+        const std::size_t h = std::hash<std::string>{}(arg);
+        boost::hash_combine(h2, h);
+      }
+      boost::hash_combine(hash, std::hash<TokenType>{}(func.Name));
+      boost::hash_combine(hash, h2);
+      boost::hash_combine(hash, std::hash<TokenType>{}(func.Operator));
+      boost::hash_combine(hash, std::hash<std::string>{}(func.Value));
+      return hash;
     }
 };
 
@@ -126,12 +128,12 @@ struct FileMetadataNode : public Node {
 template<>
 struct std::hash<FileMetadataDef>
 {
-    std::size_t operator()(const FileMetadataDef& meta) const noexcept
-    {
-        const std::size_t h1 = std::hash<TokenType>{}(meta.Property);
-        const std::size_t h2 = std::hash<TokenType>{}(meta.Operator);
-        const std::size_t h3 = std::hash<std::string>{}(meta.Value);
-        return h1 ^ ((h2 << 1) >> 1) ^ ((h3 << 1) >> 1);
+    std::size_t operator()(const FileMetadataDef& meta) const noexcept {
+      std::size_t hash = 0;
+      boost::hash_combine(hash, std::hash<TokenType>{}(meta.Property));
+      boost::hash_combine(hash, std::hash<TokenType>{}(meta.Operator));
+      boost::hash_combine(hash, std::hash<std::string>{}(meta.Value));
+      return hash;
     }
 };
 
