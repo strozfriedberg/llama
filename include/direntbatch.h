4262093ffa962fa9456c4721b7b77702981037c2
Jon Stewart <jstewart@strozfriedberg.com>
2024-08-22 11:49:07 -0400

    R - move some function defs from .cpp to header in anticipation of templatization.

--- include/direntbatch.h
+++ include/direntbatch.h
@@ -10,6 +10,20 @@
 #include "llamaduck.h"
 #include "throw.h"
 
+void appendVal(duckdb_appender& appender, const char* s);
+void appendVal(duckdb_appender& appender, uint64_t val);
+
+template<typename T>
+void append(duckdb_appender& appender, T t) {
+  appendVal(appender, t);
+}
+
+template<typename T, typename... Args>
+void append(duckdb_appender& appender, T t, Args... args) {
+  appendVal(appender, t);
+  append(appender, args...);
+}
+
 struct Dirent : public SchemaType<std::string, std::string, std::string, std::string, std::string, std::string, uint64_t, uint64_t, uint64_t, uint64_t>
 {
   static constexpr auto ColNames = {"Id",
@@ -72,7 +86,10 @@ struct DirentBatch {
 
   uint64_t NumRows = 0;
 
-  static bool createTable(duckdb_connection& dbconn, const std::string& table);
+  static bool createTable(duckdb_connection& dbconn, const std::string& table) {
+    duckdb_state state = duckdb_query(dbconn, createQuery<Dirent>(table.c_str()).c_str(), nullptr);
+    return state != DuckDBError;
+  }
 
   void add(const Dirent& dent) {
     size_t idSize = dent.Id.size() + 1;
@@ -104,6 +121,27 @@ struct DirentBatch {
     ++NumRows;
   }
 
-  void copyToDB(duckdb_appender& appender);
+  void copyToDB(duckdb_appender& appender) {
+    duckdb_state state;
+    for (unsigned int i = 0; i + 9 < OffsetVals.size(); i += 10) {
+      append(appender,
+             Buf.data() + OffsetVals[i],
+             Buf.data() + OffsetVals[i + 1],
+             Buf.data() + OffsetVals[i + 2],
+             Buf.data() + OffsetVals[i + 3],
+             Buf.data() + OffsetVals[i + 4],
+             Buf.data() + OffsetVals[i + 5],
+             OffsetVals[i + 6],
+             OffsetVals[i + 7],
+             OffsetVals[i + 8],
+             OffsetVals[i + 9]
+      );
+      state = duckdb_appender_end_row(appender);
+      THROW_IF(state == DuckDBError, "Failed call to end_row");
+    }
+    Buf.clear();
+    OffsetVals.clear();
+    NumRows = 0;
+  }
 };
 
