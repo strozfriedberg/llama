41e48f3a27c94536802c5350a5c55ad8a8adaf08
Jon Stewart <jstewart@strozfriedberg.com>
2024-08-23 00:58:17 -0400

    B - whoops, forgot to commit a header

---
+++ include/inode.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+struct Inode {
+
+  static constexpr auto ColNames = {"Id",
+                                    "Type",
+                                    "Flags",
+                                    "Addr",
+                                    "Uid",
+                                    "Gid",
+                                    "LinkTarget",
+                                    "NumLinks",
+                                    "SeqNum",
+                                    "Created",
+                                    "Accessed",
+                                    "Modified",
+                                    "Metadata"};
+
+  std::string Id;
+
+  std::string Type;
+  std::string Flags;
+
+  uint64_t Addr;
+  uint64_t Uid;
+  uint64_t Gid;
+
+  std::string LinkTarget;
+  uint64_t    NumLinks;
+  uint64_t    SeqNum;
+
+  std::string Created;
+  std::string Accessed;
+  std::string Modified;
+  std::string Metadata;
+};
+
