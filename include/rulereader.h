e348899869eabe7528484577f363fc6a607be349
Julia Paluch <julia.paluch@strozfriedberg.com>
2024-08-27 13:33:55 -0400

    R - rulereader can be header-only

--- include/rulereader.h
+++ include/rulereader.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 
+#include "lexer.h"
 #include "parser.h"
 
 class RuleReader {
@@ -16,3 +17,21 @@ private:
   std::vector<Rule> Rules;
   std::string LastError;
 };
+
+int RuleReader::read(const std::string& input) {
+  LlamaLexer lexer(input);
+  try {
+    lexer.scanTokens();
+    LlamaParser parser(input, lexer.getTokens());
+    std::vector<Rule> rules = parser.parseRules();
+    Rules.reserve(Rules.size() + rules.size());
+    Rules.insert(Rules.end(), rules.begin(), rules.end());
+  }
+  catch (UnexpectedInputError& e) {
+    LastError = e.messageWithPos();
+    return -1;
+  }
+
+  return Rules.size();
+}
+
