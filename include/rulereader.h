2826cd766333dae80067a8b100f45958476e1272
Julia Paluch <julia.paluch@strozfriedberg.com>
2024-08-28 14:29:25 -0400

    Revert "R - rulereader can be header-only"
    This reverts commit e348899869eabe7528484577f363fc6a607be349.

--- include/rulereader.h
+++ include/rulereader.h
@@ -3,7 +3,6 @@
 #include <string>
 #include <vector>
 
-#include "lexer.h"
 #include "parser.h"
 
 class RuleReader {
@@ -17,21 +16,3 @@ private:
   std::vector<Rule> Rules;
   std::string LastError;
 };
-
-int RuleReader::read(const std::string& input) {
-  LlamaLexer lexer(input);
-  try {
-    lexer.scanTokens();
-    LlamaParser parser(input, lexer.getTokens());
-    std::vector<Rule> rules = parser.parseRules();
-    Rules.reserve(Rules.size() + rules.size());
-    Rules.insert(Rules.end(), rules.begin(), rules.end());
-  }
-  catch (UnexpectedInputError& e) {
-    LastError = e.messageWithPos();
-    return -1;
-  }
-
-  return Rules.size();
-}
-
