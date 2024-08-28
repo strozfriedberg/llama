89d0cd535ca7f630f4195111863f33e415f2cf7d
Julia Paluch <julia.paluch@strozfriedberg.com>
2024-08-28 17:17:16 -0400

    R - no need for bimap actually

--- include/token.h
+++ include/token.h
@@ -71,45 +71,38 @@ enum class TokenType {
 };
 
 namespace Llama {
-  typedef boost::bimap<std::string, TokenType> bimapType;
-
-  const bimapType keywords = boost::assign::list_of<bimapType::relation>
-    ("rule", TokenType::RULE)
-    ("meta", TokenType::META)
-    ("file_metadata", TokenType::FILE_METADATA)
-    ("signature", TokenType::SIGNATURE)
-    ("grep", TokenType::GREP)
-    ("patterns", TokenType::PATTERNS)
-    ("hash", TokenType::HASH)
-    ("condition", TokenType::CONDITION)
-    ("created", TokenType::CREATED)
-    ("modified", TokenType::MODIFIED)
-    ("filesize", TokenType::FILESIZE)
-    ("filename", TokenType::FILENAME)
-    ("filepath", TokenType::FILEPATH)
-    ("id", TokenType::ID)
-    ("name", TokenType::NAME)
-    ("all", TokenType::ALL)
-    ("any", TokenType::ANY)
-    ("offset", TokenType::OFFSET)
-    ("count", TokenType::COUNT)
-    ("count_has_hits", TokenType::COUNT_HAS_HITS)
-    ("length", TokenType::LENGTH)
-    ("md5", TokenType::MD5)
-    ("sha1", TokenType::SHA1)
-    ("sha256", TokenType::SHA256)
-    ("blake3", TokenType::BLAKE3)
-    ("encodings", TokenType::ENCODINGS)
-    ("nocase", TokenType::NOCASE)
-    ("fixed", TokenType::FIXED)
-    ("and", TokenType::AND)
-    ("or", TokenType::OR)
-    ("==", TokenType::EQUAL_EQUAL)
-    ("!=", TokenType::NOT_EQUAL)
-    (">", TokenType::GREATER_THAN)
-    (">=", TokenType::GREATER_THAN_EQUAL)
-    ("<", TokenType::LESS_THAN)
-    ("<=", TokenType::LESS_THAN_EQUAL);
+  const std::unordered_map<std::string, TokenType> keywords = {
+    {"rule", TokenType::RULE},
+    {"meta", TokenType::META},
+    {"file_metadata", TokenType::FILE_METADATA},
+    {"signature", TokenType::SIGNATURE},
+    {"grep", TokenType::GREP},
+    {"patterns", TokenType::PATTERNS},
+    {"hash", TokenType::HASH},
+    {"condition", TokenType::CONDITION},
+    {"created", TokenType::CREATED},
+    {"modified", TokenType::MODIFIED},
+    {"filesize", TokenType::FILESIZE},
+    {"filename", TokenType::FILENAME},
+    {"filepath", TokenType::FILEPATH},
+    {"id", TokenType::ID},
+    {"name", TokenType::NAME},
+    {"all", TokenType::ALL},
+    {"any", TokenType::ANY},
+    {"offset", TokenType::OFFSET},
+    {"count", TokenType::COUNT},
+    {"count_has_hits", TokenType::COUNT_HAS_HITS},
+    {"length", TokenType::LENGTH},
+    {"md5", TokenType::MD5},
+    {"sha1", TokenType::SHA1},
+    {"sha256", TokenType::SHA256},
+    {"blake3", TokenType::BLAKE3},
+    {"encodings", TokenType::ENCODINGS},
+    {"nocase", TokenType::NOCASE},
+    {"fixed", TokenType::FIXED},
+    {"and", TokenType::AND},
+    {"or", TokenType::OR}
+  };
 }
 
 class LineCol {
