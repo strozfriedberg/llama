f388b0a2080bf73561393b41199b280c49f25d8b
Julia Paluch <julia.paluch@strozfriedberg.com>
2024-09-05 16:55:20 -0400

    R - rule reader should have a parser member

--- include/rulereader.h
+++ include/rulereader.h
@@ -11,8 +11,10 @@ public:
 
   const std::vector<Rule>& getRules() const { return Rules; }
   const std::string& getLastError() const { return LastError; }
+  const LlamaParser& getParser() const { return Parser; }
 
 private:
   std::vector<Rule> Rules;
   std::string LastError;
+  LlamaParser Parser;
 };
