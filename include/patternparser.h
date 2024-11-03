ddf9c556ea86ffaf4c7de853b4e83585201fd473
Julia Paluch <julia.paluch@strozfriedberg.com>
2024-11-03 15:00:54 -0500

    F - patternParser should call lg api pattern funcs

--- include/patternparser.h
+++ include/patternparser.h
@@ -1,6 +1,20 @@
 #include "lightgrep/api.h"
 
+#include "parser.h"
+
 class PatternParser {
 public:
+  PatternParser() : Pat(lg_create_pattern()), Err(nullptr) {}
+
+  ~PatternParser() {
+    lg_destroy_pattern(Pat);
+    lg_free_error(Err);
+  }
+
+  void parse(PatternDef pDef) {
+    lg_parse_pattern(Pat, pDef.Pattern.c_str(), &pDef.Options, &Err);
+  }
+private:
   LG_HPATTERN Pat;
+  LG_Error* Err;
 };
