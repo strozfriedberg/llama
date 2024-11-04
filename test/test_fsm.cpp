f1d71fce2b611dbf491b499ed9609adc04ce2aa1
Julia Paluch <julia.paluch@strozfriedberg.com>
2024-11-04 15:49:26 -0500

    t - better test for LlamaLgFsm

--- test/test_fsm.cpp
+++ test/test_fsm.cpp
@@ -9,4 +9,8 @@ TEST_CASE("initLlamaLgFsm") {
   PatternDef pDef{LG_KeyOptions{0,0,0}, "ASCII", "test"};
   LlamaLgFsm fsm;
   REQUIRE_NOTHROW(fsm.addPattern(p.parse(pDef), pDef.Encoding, 0));
+  REQUIRE(lg_fsm_pattern_count(fsm.Fsm) == 1);
+  REQUIRE(std::string(lg_fsm_pattern_info(fsm.Fsm, 0)->Pattern) == "test");
+  REQUIRE(std::string(lg_fsm_pattern_info(fsm.Fsm, 0)->EncodingChain) == "ASCII");
+  REQUIRE(lg_fsm_pattern_info(fsm.Fsm, 0)->UserIndex == 0);
 }
