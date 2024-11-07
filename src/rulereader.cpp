b98aa0d5086aa728972280c01466bd59fa537619
Julia Paluch <julia.paluch@strozfriedberg.com>
2024-11-07 16:01:40 -0500

    r - no need to pass ruleIndices vector to parseRules anymore

--- src/rulereader.cpp
+++ src/rulereader.cpp
@@ -6,7 +6,7 @@ int RuleReader::read(const std::string& input) {
   try {
     lexer.scanTokens();
     Parser = LlamaParser(input, lexer.getTokens());
-    std::vector<Rule> rules = Parser.parseRules(lexer.getRuleIndices());
+    std::vector<Rule> rules = Parser.parseRules(lexer.getRuleIndices().size());
     Rules.reserve(Rules.size() + rules.size());
     Rules.insert(Rules.end(), rules.begin(), rules.end());
   }
