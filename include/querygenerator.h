b094a2c94167fdf81c525ad3bcdb1704974fa0de
Julia Paluch <julia.paluch@strozfriedberg.com>
2025-01-07 15:30:27 -0500

    R - extract sql query generation for property node

---
+++ include/querygenerator.h
@@ -0,0 +1,28 @@
+#include <parser.h>
+
+class QueryGenerator {
+public:
+  QueryGenerator(const LlamaParser& parser) : Parser(parser) {}
+
+  std::string getSqlClause(const PropertyNode& pn) {
+    std::string query = "";
+    std::string_view propertyName = Parser.lexemeAt(pn.Value.Name);
+    query += FileMetadataPropertySqlLookup.find(propertyName)->second;
+    query += " ";
+    query += Parser.lexemeAt(pn.Value.Op);
+    query += " ";
+    std::string_view val = Parser.lexemeAt(pn.Value.Val);
+    if (Parser.Tokens[pn.Value.Val].Type == LlamaTokenType::DOUBLE_QUOTED_STRING) {
+      query += "'";
+      query += val;
+      query += "'";
+    }
+    else {
+      query += val;
+    }
+    return query;
+  }
+
+private:
+  const LlamaParser& Parser;
+};
---
+++ test/test_querygenerator.cpp
@@ -0,0 +1,13 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "lexer.h"
+#include "querygenerator.h"
+
+TEST_CASE("getPropertyNodeSqlClause) {
+  std::string input("rule MyRule { file_metadata: filesize == 123456 }");
+  LlamaParser parser(input, LlamaLexer::getTokens(input));
+  QueryGenerator qg(parser);
+  PropertyNode pn{Property{5, 6, 7}};
+  std::string expected = "Filesize == 123456"; 
+  REQUIRE(qg.getSqlClause(pn) == expected);
+}
