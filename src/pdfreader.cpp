afeec295b78461b2cb49e221b19133137a7d978c
Julia Paluch <julia.paluch@strozfriedberg.com>
2025-02-24 14:22:17 -0500

    r - remove third party pdf libs

--- src/pdfreader.cpp
+++ src/pdfreader.cpp
@@ -1,11 +1,7 @@
 #include <string>
 
 #include "pdfreader.h"
-#include "PDFDoc.h"
-#include "TextOutputDev.h"
 
 bool PDFReader::readText(std::string filePath) {
-  GString *gStr = new GString(filePath.c_str());
-  PDFDoc doc = PDFDoc(gStr);
-  return doc.okToCopy();
+  return true;
 }
