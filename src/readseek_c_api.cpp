ab571d97c4e6fc9441fef64b570652a564172a3e
Julia Paluch <julia.paluch@strozfriedberg.com>
2025-02-27 12:47:04 -0500

    b - whoops, add C API

---
+++ src/readseek_c_api.cpp
@@ -0,0 +1,27 @@
+#include "readseek_c_api.h"
+#include "readseek.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int64_t readseek_read(void* obj, size_t len, uint8_t* buf) {
+    return ((ReadSeek*)obj)->read(len, buf);
+}
+
+size_t readseek_tellg(void* obj) {
+    return ((ReadSeek*)obj)->tellg();
+}
+
+size_t readseek_seek(void* obj, size_t pos) {
+    return ((ReadSeek*)obj)->seek(pos);
+}
+
+size_t readseek_size(void* obj) {
+    return ((ReadSeek*)obj)->size();
+}
+
+
+#ifdef __cplusplus
+}
+#endif
