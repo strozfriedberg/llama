97a9a4deb48d13c26f080d434a49e94d69ba4026
Julia Paluch <julia.paluch@strozfriedberg.com>
2025-05-02 15:35:01 -0400

    t - move hashset tests

--- test/test_processor.cpp
+++ test/test_processor.cpp
@@ -14,15 +14,8 @@
 #include <fstream>
 #include <thread>
 #include <vector>
-#include <filesystem>
 
 #include "boost_asio.h"
-
-#include "boost/interprocess/file_mapping.hpp"
-#include "boost/interprocess/mapped_region.hpp"
-
-namespace bip = boost::interprocess;
-
 
 TEST_CASE("testBoostThreadPool") {
   unsigned int count = 0;
@@ -157,13 +150,4 @@ TEST_CASE("testSearchWithMultipleHits") {
   pst.createTempTableAndPopulate(expectedHits);
   REQUIRE(0 == pst.numDiffsBetweenTables());
 
-}
-
-TEST_CASE("testHashsetBundleLookup") {
-  SFHASH_HashValues Hashes;
-  std::string blake3 = "4878ca0425c739fa427f7eda20fe845f6b2e46ba5fe2a14df5b1e32f50603215";
-  sfhash_unhex(Hashes.Blake3, blake3.c_str(), blake3.size());
-
-  LlamaHashset hsetBundle("actual.hset");
-  REQUIRE(hsetBundle.lookup(Hashes.Blake3));
 }
