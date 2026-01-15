874755930884d1a6d6e422e64669342064c74fbf
Jon Stewart <jstewart@strozfriedberg.com>
2026-01-15 09:31:56 -0500

    R - use a move iterator for copying the temporary parsed rules into Rules, nice little performance win on RuleReader::read()'s own overhead

--- src/rulereader.cpp
+++ src/rulereader.cpp
@@ -7,6 +7,7 @@ bool RuleReader::read(const std::string& input, const std::string&) {
   Parser = LlamaParser(input, Lexer.tokens());
   std::vector<Rule> rules = Parser.parseRules(Lexer.ruleIndices());
   Rules.reserve(Rules.size() + rules.size());
-  Rules.insert(Rules.end(), rules.begin(), rules.end());
+  Rules.insert(Rules.end(), std::make_move_iterator(rules.begin()), std::make_move_iterator(rules.end()));
+  // Rules.insert(Rules.end(), rules.begin(), rules.end());
   return (Parser.errors().size() + Lexer.errors().size() == 0);
 }
