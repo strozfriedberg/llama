0796b270fb55f1e2e62a9c511a55a0d8d24a1a8b
Jon Stewart <jonathan.l.stewart@gmail.com>
2026-05-24 09:00:57 -0400

    test(posixreader): update Linux-only test to extents[0].InodeId
    Stale reference inside #ifdef __linux__ would have broken the Linux
    build; macOS skips this code path so the local build was green.

--- test/test_posixreader.cpp
+++ test/test_posixreader.cpp
@@ -89,7 +89,7 @@ TEST_CASE("parseExtents converts FIEMAP buffer to Extent records", "[posixreader
     REQUIRE(extents[0].LogicalEnd == 4096);
     REQUIRE(extents[0].PhysicalStart == 1000);
     REQUIRE(extents[0].PhysicalEnd == 1000 + 4096);
-    REQUIRE(extents[0].Inode == 42);
+    REQUIRE(extents[0].InodeId == "");
     REQUIRE(extents[0].Path == "/test.txt");
     REQUIRE(extents[0].Flags == "");
 
